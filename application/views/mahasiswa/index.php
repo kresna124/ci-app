106c0c486e601472d2f02c1a8ce49400172b4eda
kresna nur ito gunawan <kresna_173040129@mail.unpas.ac.id>
2019-10-19 02:30:23 +0700

    menambahkan fiture cari

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -18,9 +18,27 @@
 			<a href="<?= base_url(); ?>mahasiswa/tambah" class="btn btn-primary">Tambah Data Mahasiswa</a>
 		</div>
 	</div>
+
+	<div class="row mt-3">
+		<div class="col-md-6">
+			<form action="" method="post">
+				<div class="input-group">
+				  <input type="text" class="form-control" placeholder="Cari Cata Mahasiswa" name="keyword">
+				  <div class="input-group-append">
+				    <button class="btn btn-primary" type="submit" >cari</button>
+				  </div>
+				</div>
+			</form>
+		</div>
+	</div>
 	<div class="row mt-3">
 		<div class="col-md-6">
 			<h3>Daftar Mahasiswa</h3>
+			<?php if (empty($mahasiswa)) : ?>
+				<div class="alert alert-danger" role="alert">
+				  Data mahasiswa tidak ditemukan
+				</div>
+			<?php endif; ?>
 			<ul class="list-group">
 				<?php foreach ($mahasiswa as $mhs ) :?>
 			  <li class="list-group-item">
