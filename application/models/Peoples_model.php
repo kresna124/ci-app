8e89e56da1a85ee575321664c0731eb8ab8ed586
kresna nur ito gunawan <kresna_173040129@mail.unpas.ac.id>
2019-10-20 11:46:47 +0700

    menambahkan search pada pagination

--- application/models/Peoples_model.php
+++ application/models/Peoples_model.php
@@ -4,7 +4,11 @@ class Peoples_model extends CI_Model{
 	public function getAllPeoples(){
 		return $this->db->get('peoples')->result_array();
 	}
-	public function getPeoples($lmit, $start){
+	public function getPeoples($lmit, $start,$keyword = null){
+		if ($keyword) {
+			$this->db->like('name',$keyword);
+			$this->db->or_like('email',$keyword);
+		}
 		return $this->db->get('peoples',$lmit ,$start)->result_array();
 	}
 	public function countAllPeoples(){
--- application/views/peoples/index.php
+++ application/views/peoples/index.php
@@ -1,7 +1,20 @@
 <div class="container">
+	<h3 class="mt-3">List of People</h3>
+	<div class="row">
+		<div class="col-md-5">
+			<form action="<?= base_url('peoples') ?>" method="post">
+				<div class="input-group mb-3">
+				  <input type="text" class="form-control" placeholder="Search keyword.." name="keyword" autocomplete="off" autofocus>
+				  <div class="input-group-append">
+				    <input class="btn btn-primary" type="submit" name="submit">
+				  </div>
+				</div>
+			</form>
+		</div>
+	</div>
 	<div class="row">
 		<div class="col-md-10">
-			<h3 class="mt-3">List of People</h3>
+			<h5>Results : <?= $total_rows ?></h5>
 			<table class="table">
 				<thead>
 					<tr>
@@ -11,7 +24,17 @@
 						<th>Action</th>
 					</tr>
 				</thead>
+
 				<tbody>
+					<?php if(empty($peoples)): ?>
+						<tr>
+							<td colspan="4">
+								<div class="alert alert-danger" role="alert">
+									data not found!
+								</div>
+							</td>
+						</tr>
+					<?php endif; ?>
 					<?php foreach( $peoples as $People) : ?>
 					<tr>
 						<th><?= ++$start; ?></th>
@@ -25,6 +48,7 @@
 					</tr>
 					<?php endforeach; ?>
 				</tbody>
+
 			</table>	
 			<?= $this->pagination->create_links(); ?>
 		</div>
